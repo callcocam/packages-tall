83ebb67a3bb3ccd9d62ea108f9c0a122418fec6e
callcocam <callcocam@gmail.com>
2022-03-18 14:54:07 -0300

    searchable igual verdadeiro

--- tall-form/resources/views/fields/checkboxs.blade.php
+++ tall-form/resources/views/fields/checkboxs.blade.php
@@ -1,11 +1,17 @@
-<div class="border-4 border-dotted border-gray-300 rounded-md p-5  mt-3">
+<div class="border-l-4 border-r-4 border-b-4 border-dotted border-gray-300 rounded-md p-5 ">
+    <label class="flex flex-col space-y-2" for="{{ $field->name }}">
+        {{ $field->label }}
+        @if ($field->hint)
+            <p class="pl-2 text-gray-600 text-sm">{{ $field->hint }}</p>
+        @endif
+    </label>
     <div class="my-2">
-        <x-input wire:model.debounce.500ms="checkboxSearch.{{ $field->name }}" right-icon="search"
-            placeholder="{{ __('Search...') }}" />
+        <x-input id={{ $field->name }} wire:model.debounce.500ms="checkboxSearch.{{ $field->name }}"
+            right-icon="search" placeholder="{{ __('Search...') }}" />
     </div>
-    @forelse  ($field->options as $key => $label )
-        @include('tall-forms::fields.checkbox', compact('key','label'))
+    @forelse  ($field->options as $key => $label)
+        @include('tall-forms::fields.checkbox', compact('key', 'label'))
     @empty
-        @include('tall-forms::fields.checkbox',['label'=>$field->label, 'key'=>1])
+        @include('tall-forms::fields.checkbox', ['label' => $field->label, 'key' => 1])
     @endforelse
 </div>
--- tall-form/resources/views/fields/divider.blade.php
+++ tall-form/resources/views/fields/divider.blade.php
@@ -1,6 +1,6 @@
-<div class="bg-white rounded shadow-lg p-4 px-4 md:p-2 mb-2">
+<div class="bg-white rounded p-4 px-4 md:p-2 mb-2  border-t-2">
     <div class="text-sm ">
-        <div class="text-gray-600">
+        <div class="text-gray-600 flex flex-col space-y-1">
             <p class="font-medium text-lg">{{ $field->label }}</p>
             @if ($field->hint)
                 <p>{{ $field->hint }}</p>
