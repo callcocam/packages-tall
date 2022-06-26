02e78611a6c88b48984e3a45b69bbdb5d949a44e
Claudio Campos <cal.tj@hotmail.com>
2022-06-26 12:41:04 -0300

    id

--- tall-form/resources/views/fields/radio.blade.php
+++ tall-form/resources/views/fields/radio.blade.php
@@ -2,9 +2,9 @@
     $checked = \Arr::get($data,  $field->name) == $key;
 @endphp
 @if ($field->left_label)
-<x-radio  id="{{ $key }}" value="{{ $key }}" :lg="$field->lg" :md="$field->md" left-label="{{ __(\Str::title($label)) }}"
+<x-radio  id="{{ $field->id }}-{{ $key }}" value="{{ $key }}" :lg="$field->lg" :md="$field->md" left-label="{{ __(\Str::title($label)) }}"
     wire:model.defer="{{ $field->key }}" />
 @else
-<x-radio  id="{{ $key }}" value="{{ $key }}" :lg="$field->lg" :md="$field->md" label="{{ __(\Str::title($label)) }}"
+<x-radio  id="{{ $field->id }}-{{ $key }}" value="{{ $key }}" :lg="$field->lg" :md="$field->md" label="{{ __(\Str::title($label)) }}"
     wire:model.defer="{{ $field->key }}" />
 @endif
