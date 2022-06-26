e7a18a2ac06f54e7bd9ac742b252c43d2937187c
Claudio Campos <cal.tj@hotmail.com>
2022-06-26 12:56:41 -0300

    toggle id

--- tall-form/resources/views/fields/toggle.blade.php
+++ tall-form/resources/views/fields/toggle.blade.php
@@ -1,9 +1,10 @@
 <div class="mt-{{$field->mt}}">
     @if ($field->left_label)
-        <x-toggle :lg="$field->lg" :md="$field->md" left-label="{{ $field->label }}"
+        <x-toggle  id="{{ $field->id }}" :lg="$field->lg" :md="$field->md" left-label="{{ $field->label }}"
             wire:model.defer="{{ $field->key }}" />
     @else
-        <x-toggle :lg="$field->lg" :md="$field->md" label="{{ $field->label }}"
+        <x-toggle  id="{{ $field->id }}" :lg="$field->lg" :md="$field->md" label="{{ $field->label }}"
             wire:model.defer="{{ $field->key }}" />
     @endif
 </div>
+
