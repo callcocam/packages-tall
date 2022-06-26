389a805f6a049a8a5ee14cacad1991d92ee0d3d1
Claudio Campos <cal.tj@hotmail.com>
2022-06-26 01:58:52 -0300

    toggle

--- tall-form/resources/views/fields/toggle.blade.php
+++ tall-form/resources/views/fields/toggle.blade.php
@@ -1,6 +1,9 @@
-@if ($field->left_label)
-    <x-toggle :lg="$field->lg" :md="$field->md" left-label="{{ $field->label }}"
-        wire:model.defer="{{ $field->key }}" />
-@else
-    <x-toggle :lg="$field->lg" :md="$field->md" label="{{ $field->label }}" wire:model.defer="{{ $field->key }}" />
-@endif
+<div class="mt-{{$field->mt}}">
+    @if ($field->left_label)
+        <x-toggle :lg="$field->lg" :md="$field->md" left-label="{{ $field->label }}"
+            wire:model.defer="{{ $field->key }}" />
+    @else
+        <x-toggle :lg="$field->lg" :md="$field->md" label="{{ $field->label }}"
+            wire:model.defer="{{ $field->key }}" />
+    @endif
+</div>
