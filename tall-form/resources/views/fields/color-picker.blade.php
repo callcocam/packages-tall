7a7e7583f9fc00985a82defeb88eff9ce6fc2e07
Claudio Campos <cal.tj@hotmail.com>
2022-06-26 01:59:31 -0300

    color

--- tall-form/resources/views/fields/color-picker.blade.php
+++ tall-form/resources/views/fields/color-picker.blade.php
@@ -1,15 +1,20 @@
-@if ($field->wire_model == 'defer')
-    <x-color-picker class="w-full" wire:model.defer="{{ $field->key }}" placeholder="{{ $field->placeholder }}"
-        hint="{{ $field->hint }}" corner-hint="{{ $field->corner_hint }}" label="{{ $field->label }}">
-    </x-color-picker>
-@endif
-@if ($field->wire_model == 'lazy')
-    <x-color-picker class="w-full" wire:model.lazy="{{ $field->key }}" placeholder="{{ $field->placeholder }}"
-        hint="{{ $field->hint }}" corner-hint="{{ $field->corner_hint }}" label="{{ $field->label }}">
-    </x-color-picker>
-@endif
-@if ($field->wire_model == 'model')
-    <x-color-picker class="w-full" wire:model="{{ $field->key }}" placeholder="{{ $field->placeholder }}"
-        hint="{{ $field->hint }}" corner-hint="{{ $field->corner_hint }}" label="{{ $field->label }}">
-    </x-color-picker>
-@endif
+<div>
+    @if ($field->label)
+        <x-label>{{ $field->label }}</x-label>
+    @endif
+    @if ($field->wire_model == 'defer')
+        <x-color-picker :colors="$field->colors" class="w-full" wire:model.defer="{{ $field->key }}"
+            placeholder="{{ $field->placeholder }}" hint="{{ $field->hint }}" corner-hint="{{ $field->corner_hint }}"
+            label="{{ $field->label }}" />
+    @endif
+    @if ($field->wire_model == 'lazy')
+        <x-color-picker :colors="$field->colors" class="w-full" wire:model.lazy="{{ $field->key }}"
+            placeholder="{{ $field->placeholder }}" label="{{ $field->label }}" />
+    @endif
+    @if ($field->wire_model == 'model')
+        <x-color-picker :colors="$field->colors" class="w-full" wire:model="{{ $field->key }}"
+            placeholder="{{ $field->placeholder }}" hint="{{ $field->hint }}"
+            corner-hint="{{ $field->corner_hint }}" label="{{ $field->label }}" />
+    @endif
+</div>
+
