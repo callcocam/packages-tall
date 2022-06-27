3cee4495a41ebcd16bc24eaac80452d9cc86290f
Claudio Campos <callcocam@gmail.com>
2022-06-27 12:42:30 -0300

    tall schema

---
+++ tall-report/src/ReportServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+/**
+* Created by Claudio Campos.
+* User: callcocam@gmail.com, contato@sigasmart.com.br
+* https://www.sigasmart.com.br
+*/
+namespace Tall\Report;
+
+use Illuminate\Support\ServiceProvider;
+
+class ReportServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
